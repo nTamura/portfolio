3873a4f97ad351c2eb2837c279879910f00c468a
Nic Tamura <nictamura@gmail.com>
2017-05-29 23:18:05 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).foundation()
+function HeaderParallax() {
+  scrollPos = $(this).scrollTop();
+  $('.headline').css({
+    'margin-top': (scrollPos/4)+"px",
+    'opacity': 1-(scrollPos/250)
+  });
+}
+
+$(document).ready(function(){
+
+
+  $(window).scroll(function() {
+    HeaderParallax();
+  });
+});
