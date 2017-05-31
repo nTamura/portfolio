33cfd8a35e5da3978560d9ea76149d92cb9e70ad
Nic Tamura <nictamura@gmail.com>
2017-05-31 01:56:01 -0700

    fade in effects for showcase, tags for techs, technology list, footer contact remade, email out, download pfd docs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
 
+  $(function(){
+    $('.first').waypoint(function() {
+      $('.first').addClass('animated fadeInDown');
+      }, { offset: '50%' }
+    );
+    $('.second').waypoint(function() {
+      $('.second').addClass('animated fadeInRight');
+      }, { offset: '50%' }
+    );
+    $('.third').waypoint(function() {
+      $('.third').addClass('animated fadeInLeft');
+      }, { offset: '50%' }
+    );
+  });
+
 (function blink() {
-  $('.blink_me').fadeOut(500).fadeIn(500, blink); 
+  $('.blink_me').fadeOut(500).fadeIn(500, blink);
 })();
+
+
+
+
 
 
 });
